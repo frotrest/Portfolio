e933544e03e6b626496426c6c63839e9db094cc2
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-09-08 11:07:53 +0300

    Update Works.jsx

--- src/components/Main/Works/Works.jsx
+++ src/components/Main/Works/Works.jsx
@@ -33,7 +33,7 @@ const Works = () => {
       id: 3,
       title: 'Money Guard',
       desc: 'Personal Finance Tracker is a adaptive multipage application for tracking personal income and expenses.',
-      tags: ['React', 'CSS Modules', 'API'],
+      tags: ['React + Redux', 'TypeScript', 'CSS Modules', 'API'],
       size: styles.large,
       img: moneyguard,
       link: 'https://moneyguard-three.vercel.app/',
